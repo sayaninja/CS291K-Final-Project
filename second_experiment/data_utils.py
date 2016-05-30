aefb88b6b9414648392590274cd9ef178108e850
Zhansaya <zhansaya@umail.ucsb.edu>
2016-05-30 15:45:03 -0700

    update

--- second_experiment/data_utils.py
+++ second_experiment/data_utils.py
@@ -40,10 +40,9 @@ def get_reviews_data():
     return reviews_data
 
 def get_restaurants_data():
-    stars_converter = {0: 0, 0.5: 1, 1: 2,
-                       1.5: 3, 2: 4, 2.5: 5,
-                       3: 6, 3.5: 7, 4: 8,
-                       4.5: 9, 5: 10}
+    stars_converter = {1: 0, 1.5: 1, 2: 2,
+                       2.5: 3, 3: 4, 3.5: 5,
+                       4: 6, 4.5: 7, 5: 8}
     stars = {}
     file = open(rests_filename, "r")
     for line in file:
--- second_experiment/train.py
+++ second_experiment/train.py
@@ -1,16 +1,18 @@
+import datetime
+
 from data_utils import *
 from text_cnn import *
 
 print("Importing data...")
 x, y, vocabulary, vocabulary_inv = load_data_based_ids()
-
 print("Done importing.")
+
 # Hyper parameters
-num_validation = 1000
-num_test = 1000
-num_classes = 11
-num_epochs = 100
-batch_size = 5
+num_validation = 10
+num_test = 10
+num_classes = 9
+num_epochs = 500
+batch_size = 10
 embedding_size = 128
 filter_sizes = [3, 4, 5]
 num_filters = 128
@@ -19,19 +21,21 @@ keep_prob = 0.5
 l2_reg_lambda = 0.5
 
 # Get data
+shuffle_indices = np.random.permutation(np.arange(len(y)))
 
-x_train = x
-y_train = y
-# x_train, x_val, x_test = x_shuffled[:-(num_validation + num_test)], \
-#                          x_shuffled[-(num_validation + num_test): -num_test], \
-#                          x_shuffled[-num_test:]
-# y_train, y_val, y_test = y_shuffled[:-(num_validation + num_test)], \
-#                          y_shuffled[-(num_validation + num_test): -num_test], \
-#                          y_shuffled[-num_test:]
-#
+x_shuffled = x[shuffle_indices]
+y_shuffled = y[shuffle_indices]
+
+x_train, x_val, x_test = x_shuffled[:-(num_validation + num_test)], \
+                         x_shuffled[-(num_validation + num_test): -num_test], \
+                         x_shuffled[-num_test:]
+y_train, y_val, y_test = y_shuffled[:-(num_validation + num_test)], \
+                         y_shuffled[-(num_validation + num_test): -num_test], \
+                         y_shuffled[-num_test:]
+
 print("Vocabulary Size: {:d}".format(len(vocabulary)))
-# print("Train/Dev/Test split: {:d}/{:d}/{:d}".format(len(y_train), num_validation, num_test))
-#
+print("Train/Dev/Test split: {:d}/{:d}/{:d}".format(len(y_train), num_validation, num_test))
+
 with tf.Graph().as_default():
     sess = tf.Session()
     with sess.as_default():
@@ -61,21 +65,22 @@ with tf.Graph().as_default():
             _, step, loss, accuracy = sess.run(
                     [train_op, global_step, text_cnn.loss, text_cnn.accuracy],
                     feed_dict=feed_dict)
+            time_str = datetime.datetime.now().isoformat()
+            print("{}: step {}, loss {:g}, acc {:g}".format(time_str, step, loss, accuracy))
+
+
+        def test_step(x_batch, y_batch):
+            feed_dict = {
+                text_cnn.review_placeholder: x_batch,
+                text_cnn.stars_placeholder: y_batch,
+                text_cnn.dropout: 1
+            }
+            _, step, loss, accuracy = sess.run(
+                    [train_op, global_step, text_cnn.loss, text_cnn.accuracy],
+                    feed_dict=feed_dict)
             print("step {}, loss {:g}, acc {:g}".format(step, loss, accuracy))
 
 
-#         def test_step(x_batch, y_batch):
-#             feed_dict = {
-#                 text_cnn.review_placeholder: x_batch,
-#                 text_cnn.stars_placeholder: y_batch,
-#                 text_cnn.dropout: 1
-#             }
-#             _, step, loss, accuracy = sess.run(
-#                     [train_op, global_step, text_cnn.loss, text_cnn.accuracy],
-#                     feed_dict=feed_dict)
-#             print("step {}, loss {:g}, acc {:g}".format(step, loss, accuracy))
-#
-#
         # Start the training loop
         print "\nTraining..."
         step = 1
@@ -91,29 +96,29 @@ with tf.Graph().as_default():
             train_step(x_batch, batch_labels_one_hot)
 
         # Evaluate validation set
-#         print "\nValidation Data Evaluation..."
-#         step = 1
-#         for step in range(num_validation/batch_size):
-#             offset = (step * batch_size) % (y_val.shape[0] - batch_size)
-#
-#             # Generate a minibatch
-#             x_batch = x_val[offset:(offset + batch_size), :]
-#             y_batch = y_val[offset:(offset + batch_size)]
-#
-#             batch_labels_one_hot = np.zeros((batch_size, num_classes))
-#             batch_labels_one_hot[np.arange(batch_size), y_batch] = 1
-#             test_step(x_batch, batch_labels_one_hot)
-#
-#         # Evaluate test set
-#         print "\nTest Data Evaluation..."
-#         step = 1
-#         for step in range(num_test/batch_size):
-#             offset = (step * batch_size) % (y_val.shape[0] - batch_size)
-#
-#             # Generate a minibatch
-#             x_batch = x_test[offset:(offset + batch_size), :]
-#             y_batch = y_test[offset:(offset + batch_size)]
-#
-#             batch_labels_one_hot = np.zeros((batch_size, num_classes))
-#             batch_labels_one_hot[np.arange(batch_size), y_batch] = 1
-#             test_step(x_batch, batch_labels_one_hot)
+        print "\nValidation Data Evaluation..."
+        step = 1
+        for step in range(num_validation/batch_size):
+            offset = (step * batch_size) % (y_val.shape[0] - batch_size)
+
+            # Generate a minibatch
+            x_batch = x_val[offset:(offset + batch_size), :]
+            y_batch = y_val[offset:(offset + batch_size)]
+
+            batch_labels_one_hot = np.zeros((batch_size, num_classes))
+            batch_labels_one_hot[np.arange(batch_size), y_batch] = 1
+            test_step(x_batch, batch_labels_one_hot)
+
+        # Evaluate test set
+        print "\nTest Data Evaluation..."
+        step = 1
+        for step in range(num_test/batch_size):
+            offset = (step * batch_size) % (y_val.shape[0] - batch_size)
+
+            # Generate a minibatch
+            x_batch = x_test[offset:(offset + batch_size), :]
+            y_batch = y_test[offset:(offset + batch_size)]
+
+            batch_labels_one_hot = np.zeros((batch_size, num_classes))
+            batch_labels_one_hot[np.arange(batch_size), y_batch] = 1
+            test_step(x_batch, batch_labels_one_hot)
